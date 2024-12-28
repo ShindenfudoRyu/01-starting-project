1d1bf079184434e976fdb1cc2f8662427ab02fc1
Azhr <azhr95@hotmail.com>
2024-12-28 12:44:18 -0500

    Output EventEmitter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,15 +8,16 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) name!: string;
-  // @Input({ required: true }) avatar!: string;
-  name = input.required<string>();
-  avatar = input.required<string>();
+  @Input({required:true}) id!:string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Output() select = new EventEmitter(); // custome property to emit custom values to the parent
 
-  imagePath = computed(() => 'assets/users/' + this.avatar());
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar();
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
